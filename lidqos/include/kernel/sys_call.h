df3ba574c9df25ec3ba3253e80aeb9a8f047b038
MingYueRuYa <635672377@qq.com>
2017-09-07 22:42:43 +0800

    add IDT solve.

---
+++ lidqos/include/kernel/sys_call.h
@@ -0,0 +1,16 @@
+/*
+ * sys_call.h
+ * 
+ * 系统中断程序头文件
+ * */
+
+#ifndef _SYS_CALL_H
+#define _SYS_CALL_H
+
+#include <kernel/typedef.h>
+#include <kernel/io.h>
+#include <kernel/printf.h>
+
+void int_div_error();
+
+#endif //_SYS_CALL_H
