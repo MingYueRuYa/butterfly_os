cabef9d9254caa0dc154a3f6615864d8a4c7cba9
MingYueRuYa <635672377@qq.com>
2018-08-09 22:59:18 +0800

    add tip information.

--- compile.sh
+++ compile.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+if [ "$1"x = "clear"x ]; then
+    rm -rf ./bin
+    echo -e "\033[32m clear successful \033[0m"
+    exit
+fi
+
+
 mkdir -p bin
 
+if [ "0" -eq $? ]; then
+    echo -e "\033[32m create bin dir successful \033[0m"
+else
+    echo -e "\033[31m create bin dir error \033[0m"
+    exit
+fi
+
 nasm boot.asm -o ./bin/boot.bat
+if [ "0" -eq $? ]; then
+    echo -e "\033[32m compile boot.asm successful \033[0m"
+else
+    echo -e "\033[31m compile boot.asm error \033[0m"
+    exit
+fi
+
 javac src/OperatingSystem.java -d ./bin/
 
 cd ./bin
 java  OperatingSystem
 cd ..
-mv ./system.img ./bin/
+echo -e "\033[32m generate successful \033[0m"
