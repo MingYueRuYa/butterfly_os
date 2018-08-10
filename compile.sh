d876cad892336ca60dac38c7817f5214589b684a
MingYueRuYa <635672377@qq.com>
2018-08-10 23:07:55 +0800

    add make floppy code.

--- compile.sh
+++ compile.sh
@@ -25,8 +25,15 @@ else
 fi
 
 javac src/OperatingSystem.java -d ./bin/
+javac src/Floppy.java -d ./bin/
 
 cd ./bin
+if [ "0" -eq $? ]; then
+    echo -e "\033[32m java  OperatingSystem \033[0m"
+else
+    echo -e "\033[31m java  OperatingSystem\033[0m"
+    exit
+fi
 java  OperatingSystem
 cd ..
 echo -e "\033[32m generate successful \033[0m"
