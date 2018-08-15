10d6497e211d0b427b5a90876697d1092b23abe9
MingYueRuYa <635672377@qq.com>
2018-08-15 22:51:54 +0800

    inter protect mode.

--- kernel.asm
+++ kernel.asm
@@ -1,25 +1,92 @@
-org 0x8000
+%include "pm.inc"
 
-entry:
-    mov ax, 0
-    mov ss, ax
+org 0x9000
+
+jmp LABEL_BEGIN
+
+[SECTION .gdt]
+;                               段基址      段界限              属性
+LABEL_GDT:          Descriptor      0,          0,              0
+LABEL_DESC_CODE32:  Descriptor      0,          SegCode32Len-1, DA_C+DA_32
+LABEL_DESC_VIDEO:   Descriptor      0B8000h,    0ffffh,         DA_DRW
+
+
+GdtLen  equ $ - LABEL_GDT
+GdtPtr  dw  GdtLen - 1
+        dd  0
+
+SelectorCode32  equ LABEL_DESC_CODE32 - LABEL_GDT
+SelectorVideo   equ LABEL_DESC_VIDEO - LABEL_GDT
+
+[SECTION .s16]
+[BITS 16]
+LABEL_BEGIN:
+    mov ax, cs
     mov ds, ax
     mov es, ax
+    mov ss, ax
+    mov sp, 0100h
+
+    xor eax, eax
+    mov ax, cs
+    shl eax, 4
+    add eax, LABEL_SEG_CODE32
+    mov word [LABEL_DESC_CODE32 + 2], ax
+    shr eax, 16
+    mov byte [LABEL_DESC_CODE32 + 4], al
+    mov byte [LABEL_DESC_CODE32 + 7], ah
+
+    xor eax, eax
+    mov ax, ds
+    shl ax, 4
+    add eax, LABEL_GDT
+    mov dword [GdtPtr + 2], eax
+
+    lgdt [GdtPtr]
+
+    ; 关中断
+    cli
+
+    ; 打开A20
+    in al, 92h
+    or al, 00000010h
+    out 92h, al
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp dword SelectorCode32:0
+
+[SECTION .s32]
+[BITS 32]
+LABEL_SEG_CODE32:
+    mov ax, SelectorVideo
+    mov gs, ax
     mov si, msg
+    mov ebx, 10
+    mov ecx, 2
 
-putloop:
+showChar:
+    mov edi, (80*11)
+    add edi, ebx
+    mov eax, edi
+    mul ecx
+    mov edi, eax
+    mov ah, 0ch
     mov al, [si]
-    add si, 1
     cmp al, 0
-    je fin
-    mov ah, 0x0e
-    mov bx, 15
-    int 0x10
-    jmp putloop
-
-fin:
-    HLT
-    jmp fin
-
+    je end
+    add ebx, 1
+    add si, 1
+    mov [gs:edi], ax
+    jmp showChar
+end:
+    jmp $
 msg:
-    DB "This is Hello World from kernel"
+    DB "Protect Mode", 0
+
+SegCode32Len equ $ - LABEL_SEG_CODE32
+
+
+
