3f2126d07c222c61a52f9375c4fb72020ed999e0
MingYueRuYa <635672377@qq.com>
2018-09-06 23:17:06 +0800

    add win sheet code but not compile.

---
+++ win_sheet.c
@@ -0,0 +1,89 @@
+#include "win_sheet.h"
+#include "mem_utli.h"
+
+#define SHEET_USE 1
+struct SHEET *sheet_alloc(struct SHTCTL *ctl)
+{
+    struct SHEET *sht = 0;
+    int i = 0;
+    for (i = 0; i< MAX_SHEETS; ++i) {
+        if (ctl->sheets0[i].flags == 0) {
+            sht = &ctl->sheets0[i];
+            sht->flags  = SHEET_USE;
+            sht->height = -1;
+            return sth;
+        }
+    }
+    return sth;
+}
+
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram,
+                            int xsize, int ysize)
+{
+    struct SHTCTL *ctl;
+    int i = 0;
+    ctl = (struct SHTCTL *)memman_alloc_4k(memman, SIZE_OF_SHEET);
+    if (0 == ctl) {
+        return 0;
+    }
+
+    ctl->vram   = vram;
+    ctl->xsize  = xsize;
+    ctl->ysize  = ysize;
+    ctl->top    = -1;
+
+    for (i = 0; i < MAX_SHEETS; i++) {
+        ctl->sheets0[i].flags = 0;
+    }
+    return ctl;
+}
+
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, 
+                    int xsize, int ysize, int col_inv)
+{
+    sht->buf = buf;
+    sht->bxsize = xsize;
+    sht->bysize = ysize;
+    sht->col_inv = col_inv;
+}
+
+void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
+{
+}
+
+int sheet_refresh(struct SHTCTL *ctl)
+{
+    int h, bx, by, vx, vy;
+    unsigned char *buf, c, *vram = ctl->vram;
+    struct SHEET *sht;
+    for (h = 0; h <= ctl->top; h++) {
+        sht = ctl->sheets[h];
+        buf = sht->buf;
+        // 图层的高度
+        for (by = 0; by < sht->bysize; by++) {
+            // 转化为屏幕的y坐标
+            vy = sht->vy0 + by;
+            // 图层的宽度
+            for (bx = 0; bx < sht->bxsize; bx++) {
+                // 转化为屏幕的x坐标
+                vx = sht->vx0 + bx; 
+                // 拿出图层的像素点
+                c = buf[by*sht->bxsize+bx];
+                // 如果不是透明的就需要绘制，透明的则不需要绘制
+                if (c != sht->col_inv) {
+                    vram[vy*ctl->ysize+vx] = c;
+                }
+            } // for bx
+        } // for by
+    } // for h
+}
+
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
+{
+    sht->vx0 = vx0;
+    sht->vy0 = vy0;
+    if (sht->height >= 0) {
+        sheet_refresh(ctl);
+    }
+}
+
---
+++ win_sheet.h
@@ -0,0 +1,32 @@
+#ifndef WIN_SHEET_H
+#define WIN_SHEET_H
+
+struct SHEET {
+    unsigned char *buf;
+    int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+};
+
+#define MAX_SHEETS 256
+struct SHTCTL {
+    unsigned char *vram;
+    int xsize, ysize, top;
+    // 指向的图层指针
+    struct SHEET *sheets[MAX_SHEETS];
+    // 图层数据
+    struct SHEET sheets0[MAX_SHEETS];
+};
+
+#define SIZE_OF_SHEET 32
+#define size_of_shtctl 9232
+
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram,
+                            int xsize, int ysize);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf, 
+                    int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
+int sheet_refresh(struct SHTCTL *ctl);
+
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
+
+#endif // WIN_SHEET_H
